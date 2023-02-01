8f606ac421c2a450cc94701091aca93970d9bffd
Miguel Lopez <emmelop@outlook.com>
2023-02-01 23:23:16 +0100

    anonimize key

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name   = "rg-tf-multitstate"
     storage_account_name  = "sttfmultitstate19648"
     container_name        = "tf-multitstate"
-    key                   = "EG+HxMWvOV22qFeeY2wQYejxGT/5M2dqYFFt+Ikc0/KjSmW+aKqo4eSftW54d4/AsAe0ZmejNZK0+AStX8RMBg=="
+    key                   = "xxx"
   }
 }
